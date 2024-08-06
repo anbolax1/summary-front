8f1597296db6b602d0d8630e5322ee137cfcfdc4
Andrey <andrej.bockow@yandex.ru>
2024-08-06 17:23:36 +0300

    добавил аналитику от vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights/>
+        <Analytics/>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@
 // import dynamic from "next/dynamic";
 
 // const AdminApp = dynamic(() => import("@/components/AdminApp"), { ssr:false })
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react";
 import "./page.scss";
 
 export default function Home() {
